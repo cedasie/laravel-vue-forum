a3ce11e1937a66b1959edb5f5437ae75767038d1
cedasie <conquete.asie@gmail.com>
2023-08-15 21:22:22 +0200

    configuration forum

---
+++ resources/js/Pages/Forum/Index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Forum" />
+
+    <AuthenticatedLayout>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
