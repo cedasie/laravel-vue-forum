2f2bdcb8d63d0d409eb4e7c4c714f43f6d099b5b
cedasie <conquete.asie@gmail.com>
2023-08-21 17:56:35 +0200

    add pin to discussion

--- resources/js/Components/Forum/Discussion.vue
+++ resources/js/Components/Forum/Discussion.vue
@@ -5,7 +5,12 @@
                 <div class="flex items-center space-x-3">
                     <span class="inline-flex items-center rounded-lg bg-gray-100 px-3 py-0.5 text-sm text-gray-600">{{
                         discussion.topic.title }}</span>
-                    <h1 class="text-lg font-medium"> {{ discussion.title }}</h1>
+                    <h1 class="text-lg font-medium">
+                        <template v-if="discussion.is_pinned">
+                            [Pinned]
+                        </template>
+                        {{ discussion.title }}
+                    </h1>
                 </div>
             </div>
             <div>
