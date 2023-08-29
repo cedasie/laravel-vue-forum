62236752573cea765a6c07a8a2670ec2abe2bfde
cedasie <conquete.asie@gmail.com>
2023-08-29 23:41:04 +0200

    add DiscussionShowController and show page

--- resources/js/Components/Forum/Discussion.vue
+++ resources/js/Components/Forum/Discussion.vue
@@ -1,26 +1,27 @@
 <template>
-    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900 flex items-center space-x-6">
-            <div class="flex-grow">
-                <div class="flex items-center space-x-3">
-                    <span class="inline-flex items-center rounded-lg bg-gray-100 px-3 py-0.5 text-sm text-gray-600">{{
-                        discussion.topic.title }}</span>
-                    <h1 class="text-lg font-medium">
-                        <template v-if="discussion.is_pinned">
-                            [Pinned]
-                        </template>
-                        {{ discussion.title }}
-                    </h1>
-                </div>
-            </div>
-            <div>
-                avatars
+    <Link :href="route('discussions.show', discussion)" class="block bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="p-6 text-gray-900 flex items-center space-x-6">
+        <div class="flex-grow">
+            <div class="flex items-center space-x-3">
+                <span class="inline-flex items-center rounded-lg bg-gray-100 px-3 py-0.5 text-sm text-gray-600">{{
+                    discussion.topic.title }}</span>
+                <h1 class="text-lg font-medium">
+                    <template v-if="discussion.is_pinned">
+                        [Pinned]
+                    </template>
+                    {{ discussion.title }}
+                </h1>
             </div>
         </div>
+        <div>
+            avatars
+        </div>
     </div>
+    </Link>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3'
 defineProps({
     discussion: Object
 })
